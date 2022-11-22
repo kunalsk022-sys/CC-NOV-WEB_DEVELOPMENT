576fa11f2672e4a1efb44f96356d99fe89d06099
kunalsk022-sys <118584391+kunalsk022-sys@users.noreply.github.com>
2022-11-22 09:30:45 +0530

    Online Music Streaming Application
    Online Music Streaming Application, streams music online, here you can play music like any other platform Spotify, Ganna.com or Jio Savan.
    
    Co-Authored-By: kunalsk022-sys <118584391+kunalsk022-sys@users.noreply.github.com>

---
+++ TASK2-ONLINE-MUSIC-STREAMING-APPLICATION/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'convertapi/convertapi-php' => array(
+            'pretty_version' => 'v1.6.1',
+            'version' => '1.6.1.0',
+            'reference' => 'c17fb7b277da956f5335c84805f92a8389298e5f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../convertapi/convertapi-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
